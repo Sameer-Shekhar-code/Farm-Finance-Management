df3dbef5c007f2474e5c0eed52d9e4c9c9da81c4
sameer-shekhar-cwm <147237623+sameer-shekhar-cwm@users.noreply.github.com>
2025-05-06 12:56:31 +0530

    Setting up folders

---
+++ backend/farm_backend/src/test/java/com/farm/farm_backend/FarmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farm.farm_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
